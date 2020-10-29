040222bfd3b12b1ddae3e5be9ac2ebbab14065d9
Paul Chávez <paulchavezromero20@gmail.com>
2020-10-28 20:13:01 -0500

    references.js added, has the DOM references

---
+++ src/js/references.js
@@ -0,0 +1,6 @@
+// References
+export const form              = document.querySelector( '#formulario' ),
+             currency          = document.querySelector( '#moneda' ),
+             cryptocurrency    = document.querySelector( '#criptomoneda' ),
+             result            = document.querySelector( '#resultado' );
+
