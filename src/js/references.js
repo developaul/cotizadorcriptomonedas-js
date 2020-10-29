c8fde1544c7e56853ed6e9644e62972a739ff2b1
Paul Chávez <paulchavezromero20@gmail.com>
2020-10-28 20:27:50 -0500

    instance ui added

--- src/js/references.js
+++ src/js/references.js
@@ -1,6 +1,10 @@
+import UI from './classes/ui.class.js';
+
 // References
 export const form              = document.querySelector( '#formulario' ),
              currency          = document.querySelector( '#moneda' ),
              cryptocurrency    = document.querySelector( '#criptomoneda' ),
              result            = document.querySelector( '#resultado' );
 
+// Instances
+export const ui = new UI();
